7ca6875779bca9b7902a0a770a0e9baa397c3764
hanseojeong <131870768+hannodada@users.noreply.github.com>
2024-01-09 12:40:43 +0900

    Add files via upload

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    // impl 은 추상화에도 의존하고 구체화에도 의존 -> DIP 위반
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
