e1271ad6d55eb5da7aaf6dafb0b9e23cc1114507
ssb <sbexclusvie9@gmail.com>
2024-01-14 20:31:26 +0900

    - ~세션6

--- src/test/java/hello/core/singleton/SingletonService.java
+++ src/test/java/hello/core/singleton/SingletonService.java
@@ -2,10 +2,10 @@ package hello.core.singleton;
 
 public class SingletonService {
 
-    // 1. static 영역에 객체를 딱 1개만 생선한다.
+    // 1. static 영역에 객체를 딱 1개만 생성한다.
     private static final SingletonService instance = new SingletonService();
 
-    // 2. public으로 열어서 객체 인스턴스가 필요하면 이 static 메ㅓ드를 통해서만 조회하도록 허용한다.
+    // 2. public으로 열어서 객체 인스턴스가 필요하면 이 static 메서드를 통해서만 조회하도록 허용한다.
     public static SingletonService getInstance(){
         return instance;
     }
